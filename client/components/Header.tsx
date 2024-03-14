d67c668cf9ee84da045403e05268d87fee30bcb5
Matthew Nanthameechai <mnanthameechai@gmail.com>
2024-03-15 11:47:44 +1300

    api working

--- client/components/Header.tsx
+++ client/components/Header.tsx
@@ -1,8 +1,13 @@
+import { useQuery } from "@tanstack/react-query"
+import { getDadJoke } from "../api/dadjokeapi"
+
 function Header () {
+  const { data } = useQuery({ queryKey: ['joke'], queryFn: () => getDadJoke() })
   return (
     <>
     <div className="header-container">
       <h1> 🤣🤣 Lingo Laughs 🤣🤣</h1>
+      <h1>{data?.attachments[0].text}</h1>
     </div>
     </>
   )
