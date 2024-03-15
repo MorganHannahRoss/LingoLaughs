291c024c9f17d2e83c0a9cc045c4ff6252ea0c76
Harrison Rogers <110720339+HarrisonRogers@users.noreply.github.com>
2024-03-15 13:42:58 +1300

    fixed the button for the dad jokes api
    Co-authored-by: Harrison Rogers <HarrisonRogers@users.noreply.github.com>
    Co-authored-by: Matthew <matthew-nanthameechai@users.noreply.github.com>
    Co-authored-by: berhane0307 <berhane0307@users.noreply.github.com>

--- client/components/DadJoke.tsx
+++ client/components/DadJoke.tsx
@@ -2,13 +2,14 @@ import { useQuery } from '@tanstack/react-query'
 import { getDadJoke } from '../api/dadjokeapi'
 
 function DadJoke() {
-  const { data } = useQuery({ queryKey: ['joke'], queryFn: () => getDadJoke() })
+  const { data, refetch } = useQuery({ queryKey: ['joke'], queryFn: () => getDadJoke() })
+
   return (
     <>
       <div className="translation-container">
         <div className="input-text">
-          <h1>{data?.attachments[0].text}</h1>
-          <button onClick={getDadJoke}>New Joke</button>
+          <h1 className="Joke-text">{data?.attachments[0].text}</h1>
+          <button type='submit' className='input-text button' onClick={refetch}>New Joke</button>
         </div>
       </div>
     </>
